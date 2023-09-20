f074e1c56afd56d7612b7426453dd9ea63a896cb
sso9594 <sso9594@naver.com>
2023-09-20 12:27:55 +0900

    update A* Algorithm

--- Aalgorithm.py
+++ Aalgorithm.py
@@ -10,5 +10,85 @@ grid = np.array([
     [0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
     [1,0,0,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-    
+    [1,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1,1,1,0,1],
+    [1,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1,0,0,0,0],
+    [1,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1,0,0,0,0],
+    [0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1],
+    [0,0,0,0,0,0,0,0,1,0,0,1,1,1,1,0,1,1,1,0],
+    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,1,1,1,0,0],
+    [0,0,1,1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ])
+
+start = (0,0)
+goal = (0,19)
+
+def heuristic(a,b):
+    return np.sqrt((b[0]-a[0]) ** 2 + (b[1]-a[1]) ** 2)
+
+def Astar(array, start, goal):
+    neighbors = [(0,1), (0,-1), (1,0), (-1,0), (1,1), (1,-1), (-1,1), (-1,-1)]
+    close_set = set()
+    came_from = {}
+    gscore = {start:0}
+    fscore = {start:heuristic(start,goal)}
+    oheap = []
+    heapq.heappush(oheap, (fscore[start],start))
+
+    while oheap:
+        current = heapq.heappop(oheap)[1]
+        if current == goal:
+            data = []
+            while current in came_from:
+                data.append(current)
+                current = came_from[current]
+            return data
+        close_set.add(current)
+        
+        for i, j in neighbors:
+            neighbor = current[0] + i, current[1] + j
+            if 0 <= neighbor[0] < array.shape[0]:
+                if 0 <= neighbor[1] < array.shape[1]:
+                    if array[neighbor[0]][neighbor[1]] == 1:
+                        continue
+                else:
+                    continue
+            else:
+                continue
+
+            temp_g_score = gscore[current] + heuristic(current, neighbor)
+            if neighbor in close_set and temp_g_score >= gscore.get(neighbor, 0):
+                continue
+
+            if temp_g_score < gscore.get(neighbor, 0) or neighbor not in [i[1]for i in oheap]:
+                came_from[neighbor] = current
+                gscore[neighbor] = temp_g_score
+                fscore[neighbor] = temp_g_score + heuristic(neighbor, goal)
+                heapq.heappush(oheap, (fscore[neighbor], neighbor))
+    return False
+
+route = Astar(grid, start, goal)
+route = route + [start]
+route = route[::-1]
+print('경로: ',route)
+
+x_coords = []
+y_coords = []
+for i in (range(0,len(route))):
+    x = route[i][0]
+    y = route[i][1]
+    x_coords.append(x)
+    y_coords.append(y)
+
+fig, ax = plt.subplots(figsize=(12,12))
+ax.imshow(grid, cmap=plt.cm.Pastel1)
+ax.scatter(start[1],start[0], marker="*", color = "red", s=200)
+ax.scatter(goal[1],goal[0], marker="*", color = "green", s=200)
+ax.plot(y_coords, x_coords, color= "blue")
+plt.show()
